88c4a55346f144ce90dec7f591e13b538d6ef468
Ferm0494 <ferm0494@gmail.com>
2020-06-12 09:57:03 -0600

    GemFile Config SQlite to dev sec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
-  validates :email,:username, presence: true, uniqueness: true
+  validates :email, :username, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
